d7ab7e7a0feace56f7c9f36186fe1c4c4efca872
mat <mat@northwestern.edu>
2023-11-08 15:49:33 -0500

    Update Cognito pool app client id.

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,5 +1,5 @@
 export default {
   REGION: "us-east-1",
   USER_POOL_ID: "us-east-1_jHOivWnyq",
-  USER_POOL_APP_CLIENT_ID: "2ffm4f6hfsqaal7jautdb8t55u",
+  USER_POOL_APP_CLIENT_ID: "2lbmrro710pmklg30j2gns5t67",
 };
