b2d733992ec2e6d54315a5841d9cd5a4065f324b
Karen Shaw <karendid@gmail.com>
2023-12-19 18:28:47 +0000

    Fix typo in API url

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -3,5 +3,5 @@ export default {
   USER_POOL_ID: "us-east-1_jHOivWnyq",
   USER_POOL_APP_CLIENT_ID: "2lbmrro710pmklg30j2gns5t67",
   API_GATEWAY_ENDPOINT:
-    "https://admin-maktaba.api.rdc-staging.library.northwestern.edu",
+    "https://admin-maktaba-api.rdc-staging.library.northwestern.edu",
 };
