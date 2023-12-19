c85fb3c66ac51ec153d5ae4fcdae7a435be27a42
Karen Shaw <karendid@gmail.com>
2023-12-19 16:53:24 +0000

    Update API endpoint

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -3,5 +3,5 @@ export default {
   USER_POOL_ID: "us-east-1_jHOivWnyq",
   USER_POOL_APP_CLIENT_ID: "2lbmrro710pmklg30j2gns5t67",
   API_GATEWAY_ENDPOINT:
-    "https://67qdcv50f4.execute-api.us-east-1.amazonaws.com/prod",
+    "https://admin-maktaba.api.rdc-staging.library.northwestern.edu",
 };
