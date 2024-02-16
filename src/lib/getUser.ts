4a55b2a3b7f03e2b7361e0da59f5c7bb661ee5f3
Mat Jordan <mat@northwestern.edu>
2024-02-16 15:59:49 -0500

    Resolve typescript issues.

--- src/lib/getUser.ts
+++ src/lib/getUser.ts
@@ -1,4 +1,5 @@
-const isAdminUser = (user: User) => {
+const isAdminUser = (user: any) => {
+  // @ts-ignore
   const groups =
     user?.signInUserSession?.accessToken?.payload["cognito:groups"];
   return groups?.includes("Admin");
